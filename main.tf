bf6c6587288e7f68bb4a8f3acc0b96b65e731893
Cheulong Sear <searcheulong@gmail.com>
2025-06-09 16:28:15 +0700

    fix  [main.tf]: commandOptions

--- main.tf
+++ main.tf
@@ -8,10 +8,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name = "tfstateRG01"
-    storage_account_name = "tfstate0126626"
-    container_name = "pipetfstate"
-    key = "dev.tfstate"
+
   }
 }
 
