b4da48446c1c1741a282aab88c58e331504b4b1b
Cheulong Sear <searcheulong@gmail.com>
2025-06-10 01:18:03 +0700

    Prod to main (#4)
    * edit  pipeline
    
    * edit  pipeline
    
    * Staging to prod (#3)
    
    * update [main.tf]: use env from var group
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * fix  [main.tf]: commandOptions
    
    * Update azure-tf-asian-food-dev.yml for Azure Pipelines
    
    * fix  [main.tf]: commandOptions
    
    * edit  pipeline
    
    * edit  pipeline
    
    * edit  main
    
    * edit  main

---
+++ main.tf
@@ -0,0 +1,33 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+  }
+}
+
+# Declare the variable for environment
+variable "env" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "development"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources-${var.env}"
+  location = "Southeast Asia"
+  tags = {
+    environment = var.env
+  }
+}
